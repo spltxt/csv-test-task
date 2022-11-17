c584115b05ae828eafef961f4321c8a8f8e3ac5b
spltxt <ionozort3999@gmail.com>
2022-11-17 17:51:02 +0300

    added deployment files

--- core/forms.py
+++ core/forms.py
@@ -4,3 +4,4 @@ from django import forms
 class UploadForm(forms.Form):
     csv_file = forms.FileField(required=False, widget=forms.FileInput(attrs={'class': 'form-control', 'placeholder':
         'Upload "products.csv"', 'help_text': 'Choose a .csv file with products to enter'}))
+
